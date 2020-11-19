0ece227041d397c618d2f475cb3b44723064b997
guilherme lima <guilima90@hotmail.com>
2020-11-19 16:46:28 -0300

    ex2day29almost

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails
+  resources :ingredients
+
+  # criar cocktails controler com 
+  # rails generate controller cocktails
+  # codar controller para as ações pedidas (get index, show specific cocktail, create and post new cocktail)
+  # codar o view
+
 end
